66ebf7373e64c1eff26184cbe053f6ec3703f4c6
terppa90 <teromese@hotmail.com>
2023-07-18 10:50:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+import userRouter from './routes/user.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+// cors need to be above routes
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use('/posts', postRoutes);
+app.use('/user', userRouter);
+
+// const CONNECTION_URL =
+//   'mongodb+srv://terppa:terppa666@cluster0.bkqsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on ${PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

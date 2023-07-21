65ccc67c626cd92fbda8ddf7ab0cfc33b67203f6
terppa90 <teromese@hotmail.com>
2023-07-21 11:45:22 +0300

    Commit

--- index.js
+++ index.js
@@ -14,15 +14,20 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 // cors need to be above routes
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('APP IS RUNNING');
+});
 
 // const CONNECTION_URL =
 //   'mongodb+srv://terppa:terppa666@cluster0.bkqsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
@@ -39,3 +44,8 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+// package.json ->
+// "scripts": {
+//   "start": "nodemon index.js"
+// },
